f79abe77c9e73e1ce345b7ecec6393c46933a98f
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-27 16:13:29 +0530

    create profiles controller methods fetchAll, fetchOne, create, update, delete; created DTOs for createProfile & updateProfile;

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  // GET /profiles
+  @Get()
+  fetchAll(@Query('location') location: string) {
+    return [{ location }];
+  }
+
+  // GET /profiles/:id
+  @Get(':id')
+  fetchOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto): CreateProfileDto {
+    return {
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+    };
+  }
+
+  // PUT /profiles/:id
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ): UpdateProfileDto | { id: string } {
+    return {
+      id,
+      name: updateProfileDto.name,
+      description: updateProfileDto.description,
+    };
+  }
+
+  // DELETE /profiles/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {}
+}

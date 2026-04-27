d367f90f1302f036458e84e4035597e3f6ec04dc
pankajnelsontirkey <pankajnelsontirkey@outlook.com>
2026-04-27 16:36:58 +0530

    created profilesService; created dummy profiles in service file; used profilesService in fetchAll, fetchOne methods;

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+  private profiles = [
+    {
+      id: randomUUID(),
+      name: 'Pankaj Nelson',
+      description:
+        'Looking for someone to pair program with. And duo in Valorant.',
+    },
+    {
+      id: randomUUID(),
+      name: 'Nelson Tirkey',
+      description:
+        'Just looking for someone to put the semi-colons at the end of my statements;',
+    },
+    {
+      id: randomUUID(),
+      name: 'Pankaj Tirkey',
+      description: 'Just need someone to help with my deployments.',
+    },
+  ];
+
+  findAll() {
+    return this.profiles;
+  }
+
+  findById(id: string) {
+    return this.profiles.find((profile) => profile.id === id);
+  }
+}
